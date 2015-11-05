af73888455671f051299d0647dc1b204c830db8a
Adriano Valente <dri9595@gmail.com>
2015-11-05 09:03:07 -0200

    First commit - adding basic files

---
+++ app/src/main/java/com/example/adriano/esteticaregenere_android/HomeHeaderView.java
@@ -0,0 +1,34 @@
+package com.example.adriano.esteticaregenere_android;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Adriano on 10/28/15.
+ */
+public class HomeHeaderView extends LinearLayout {
+
+    public HomeHeaderView(Context context) {
+        super(context);
+    }
+
+    public HomeHeaderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.home_header_view, this, true);
+    }
+
+
+    public void updateWithData(String name, int appointments) {
+        ((TextView) findViewById(R.id.headerView_name)).setText("Olá, " + name);
+        ((TextView) findViewById((R.id.headerView_appointments))).setText("Essa semana você tem " + appointments + "consultas");
+    }
+
+}
+
+
