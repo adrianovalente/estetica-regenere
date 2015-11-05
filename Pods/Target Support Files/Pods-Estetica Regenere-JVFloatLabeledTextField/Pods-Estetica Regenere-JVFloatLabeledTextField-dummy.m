7ab404974806c617f4ab4ef709d9833bb252651f
Adriano Valente <dri9595@gmail.com>
2015-11-05 14:43:27 -0200

    Removing Pods from git tracking

--- Pods/Target Support Files/Pods-Estetica Regenere-JVFloatLabeledTextField/Pods-Estetica Regenere-JVFloatLabeledTextField-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Estetica_Regenere_JVFloatLabeledTextField : NSObject
-@end
-@implementation PodsDummy_Pods_Estetica_Regenere_JVFloatLabeledTextField
-@end
